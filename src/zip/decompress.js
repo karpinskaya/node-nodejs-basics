481296f0b49e3bf456b4ec3c7236cc24daad4711
karpinskaya <kivvvimail@gmail.com>
2024-01-23 17:43:30 +0300

    feat: implement zip function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import * as zlib from 'zlib';
+import { pipeline } from 'stream/promises';
+import * as fs from 'fs';
+import { getDirname } from '../utils.js';
+
 const decompress = async () => {
-    // Write your code here 
+    const __dirname = getDirname(import.meta.url);
+    const srcPath = `${__dirname}/files/archive.gz`;
+    const dstPath = `${__dirname}/files/fileToCompress.txt`;
+    const unzip = zlib.createGunzip();
+
+    try {
+        await pipeline(
+            fs.createReadStream(srcPath),
+            unzip,
+            fs.createWriteStream(dstPath)
+        );
+    } catch (e) {
+        throw e;
+    }
 };
 
 await decompress();
