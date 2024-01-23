e90cee8c83102a3e94d439de1cb983a4a6fccbaa
karpinskaya <kivvvimail@gmail.com>
2024-01-23 18:02:51 +0300

    feat: implement child processes functions

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,12 @@
+import { fork } from 'child_process';
+import { getDirname } from '../utils.js';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const __dirname = getDirname(import.meta.url);
+    const scriptPath = `${__dirname}/files/script.js`;
+
+    fork(scriptPath, args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2', 'someArgument3']);
