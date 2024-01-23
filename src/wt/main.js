a455c8d7787d2cb0fb760b99ebc27d2f2b24749b
karpinskaya <kivvvimail@gmail.com>
2024-01-23 17:58:38 +0300

    feat: implement worker threads functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,30 @@
+import os from 'os';
+import { Worker } from 'worker_threads';
+import { getDirname } from '../utils.js';
+
 const performCalculations = async () => {
-    // Write your code here
+    const __dirname = getDirname(import.meta.url);
+    const workerPath = `${__dirname}/worker.js`;
+
+    const workers = os.cpus().map((value, index) => {
+        return new Promise((resolve, reject) => {
+            const worker = new Worker(workerPath, {
+                workerData: index + 10,
+            });
+            worker.on('message', resolve);
+            worker.on('error', reject);
+            worker.on('exit', reject);
+        });
+    });
+
+    const workersResult = await Promise.allSettled(workers);
+
+    const result = workersResult.map(({ status, value }) => ({
+        status: status === 'fulfilled' ? 'resolved' : 'error',
+        data: value ? value : null,
+    }));
+
+    console.log(result);
 };
 
 await performCalculations();
