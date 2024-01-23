69405d875436231b528983b32615e931751ec397
karpinskaya <kivvvimail@gmail.com>
2024-01-23 17:27:21 +0300

    feat: implement calculate hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { readFile } from 'fs/promises';
+import { createHmac } from 'crypto';
+import { getDirname } from '../utils.js';
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const __dirname = getDirname(import.meta.url);
+        const filePath = `${__dirname}/files/fileToCalculateHashFor.txt`;
+        const content = await readFile(filePath, { encoding: 'utf8' });
+        const hash = createHmac('sha256', content).digest('hex');
+
+        console.log(hash);
+    } catch (e) {
+        console.log(e);
+    }
 };
 
 await calculateHash();
